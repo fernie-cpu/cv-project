7b7f795ce6e1b918c35f6cdeceaa48def15ff811
fernie-cpu <yoloisjustanick@gmail.com>
2021-02-23 11:51:56 -0300

    added react to pdf

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import './style/style.css';
+import ReactToPdf from 'react-to-pdf';
 import Header from './components/Header';
 import General from './components/General';
 import Educational from './components/Educational';
 import Experience from './components/Experience';
 
+const ref = React.createRef();
+
 function App() {
   return (
     <div>
       <Header />
-      <General />
-      <Educational />
-      <Experience />
+      <ReactToPdf targetRef={ref} filename='div-blue.pdf'>
+        {({ toPdf }) => (
+          <div className='pdfDiv'>
+            <p>Click here to download your resume when you're finished -> </p>
+            <button className='pdfBtn' onClick={toPdf}>
+              Generate pdf
+            </button>
+          </div>
+        )}
+      </ReactToPdf>
+      <div ref={ref}>
+        <General />
+        <Educational />
+        <Experience />
+      </div>
     </div>
   );
 }
